caace412ec2832ff16749d643d67cb1c8d8bdfc3
Kelci Mohrman <kmohrman@nd.edu>
2023-08-08 23:54:34 -0400

    Initial commit

---
+++ scikit_hist/multilepeft.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import numpy as np
+import awkward as ak
+np.seterr(divide='ignore', invalid='ignore', over='ignore')
+from coffea import processor
+from coffea.analysis_tools import PackedSelection
+
+from topcoffea.modules.GetValuesFromJsons import get_lumi
+#from topcoffea.modules.HistEFT import HistEFT
+import topcoffea.modules.eft_helper as efth
+import topcoffea.modules.objects as obj
+
+import hist
+from topcoffea.modules.histEFT import HistEFT
+
+class AnalysisProcessor(processor.ProcessorABC):
+    def __init__(self, samples, wc_names_lst=[]):
+        self._samples = samples
+        self._wc_names_lst = wc_names_lst
+
+        # Create the histograms with old coffea hist
+        #self._accumulator = processor.dict_accumulator({
+            #'j0pt': HistEFT("Events", wc_names_lst, hist.Cat("sample", "sample"), hist.Bin("j0pt", "Leading jet $p_{T}$ (GeV)", 10, 0, 600)),
+        #})
+
+        # Create the histograms with new scikit hist
+        self._histo_dict = {
+            "j0pt": HistEFT(
+                hist.axis.StrCategory([], growth=True, name="process"),
+                hist.axis.Regular(name="j0pt",label="Leading jet $p_{T}$ (GeV)", bins=10, start=0, stop=600, flow=True),
+                wc_names = self._wc_names_lst,
+                label="Events",
+            )
+        }
+
+
+    @property
+    def columns(self):
+        return self._columns
+
+    def process(self, events):
+
+        # Dataset parameters
+        dataset = events.metadata['dataset']
+        hist_axis_name = self._samples[dataset]["histAxisName"]
+        year   = self._samples[dataset]['year']
+        xsec   = self._samples[dataset]['xsec']
+        sow    = self._samples[dataset]['nSumOfWeights' ]
+
+        # Extract the EFT quadratic coefficients and optionally use them to calculate the coefficients on the w**2 quartic function
+        # eft_coeffs is never Jagged so convert immediately to numpy for ease of use.
+        eft_coeffs = ak.to_numpy(events['EFTfitCoefficients']) if hasattr(events, "EFTfitCoefficients") else None
+        if eft_coeffs is not None:
+            # Check to see if the ordering of WCs for this sample matches what want
+            if self._samples[dataset]['WCnames'] != self._wc_names_lst:
+                eft_coeffs = efth.remap_coeffs(self._samples[dataset]['WCnames'], self._wc_names_lst, eft_coeffs)
+
+        # Initialize objects
+        ele  = events.Electron
+        mu   = events.Muon
+        jets = events.Jet
+
+
+        ######## Lep selection  ########
+
+        e_selec = ((ele.pt>20) & (abs(ele.eta)<2.5))
+        m_selec = ((mu.pt>20) & (abs(mu.eta)<2.5))
+        leps = ak.concatenate([ele[e_selec],mu[m_selec]],axis=1)
+
+
+        ######## Jet selection  ########
+
+        jets = jets[(jets.pt>30) & (abs(jets.eta)<2.5)]
+        jets_clean = jets[obj.isClean(jets, leps, drmin=0.4)]
+        j0 = jets_clean[ak.argmax(jets.pt,axis=-1,keepdims=True)]
+
+
+        ######## Event selections ########
+
+        nleps = ak.num(leps)
+        njets = ak.num(jets_clean)
+
+        at_least_two_leps = ak.fill_none(nleps>=2,False)
+        at_least_two_jets = ak.fill_none(njets>=2,False)
+
+        selections = PackedSelection()
+        selections.add('2l2j', at_least_two_leps & at_least_two_jets)
+
+
+        ######## Normalization ########
+
+        # Normalize by (xsec/sow)
+        lumi = 1000.0*get_lumi(year)
+        norm = (xsec/sow)*lumi
+        wgts = norm*np.ones_like(events['event'])
+
+
+        ######## Fill histos ########
+
+        hout = {"j0pt": self._histo_dict["j0pt"]}
+
+        event_selection_mask = selections.all("2l2j")
+        hout["j0pt"].fill(
+            process   = hist_axis_name,
+            j0pt      = j0.pt[event_selection_mask],
+            eft_coeff = eft_coeffs[event_selection_mask],
+        )
+
+        return hout
+
+    def postprocess(self, accumulator):
+        return accumulator
+
---
+++ scikit_hist/run_multilepeft.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import json
+import cloudpickle
+import gzip
+import os
+
+from coffea import processor
+from coffea.nanoevents import NanoAODSchema
+
+import multilepeft
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser(description='You can customize your run')
+    parser.add_argument('jsonfile'       , nargs='?', default=''           , help = 'Json file(s) containing files and metadata')
+    parser.add_argument('--nworkers','-n' , default=8  , help = 'Number of workers')
+    parser.add_argument('--chunksize','-s', default=100000  , help = 'Number of events per chunk')
+    parser.add_argument('--nchunks','-c'  , default=None  , help = 'You can choose to run only a number of chunks')
+    parser.add_argument('--outname','-o'  , default='plotsTopEFT', help = 'Name of the output file with histograms')
+    parser.add_argument('--treename'      , default='Events', help = 'Name of the tree inside the files')
+    parser.add_argument('--wc-list'       , action='extend', nargs='+', help = 'Specify a list of Wilson coefficients to use in filling histograms.')
+
+    args = parser.parse_args()
+    nworkers  = int(args.nworkers)
+    chunksize = int(args.chunksize)
+    nchunks   = int(args.nchunks) if not args.nchunks is None else args.nchunks
+    json_file = args.jsonfile
+    outname   = args.outname
+    treename  = args.treename
+
+    # Load samples from json and setup the inputs to the processor
+    samplesdict = {}
+    json_file_name = json_file.split(".json")[0] # Drop the .json
+    with open(json_file) as jf:
+        samplesdict[json_file_name] = json.load(jf)
+    flist = {json_file_name: samplesdict[json_file_name]["files"]}
+
+    #wc_lst = args.wc_list if args.wc_list is not None else []
+    wc_lst = samplesdict[json_file_name]["WCnames"]
+
+    # Run the processor and get the output
+    processor_instance = multilepeft.AnalysisProcessor(samplesdict,wc_lst)
+    exec_instance      = processor.FuturesExecutor(workers=nworkers)
+    runner             = processor.Runner(exec_instance, schema=NanoAODSchema, chunksize=chunksize, maxchunks=nchunks)
+    output             = runner(flist, treename, processor_instance)
+
+    # Save the output
+    outpath = "."
+    if not os.path.isdir(outpath): os.system("mkdir -p %s"%outpath)
+    out_pkl_file = os.path.join(outpath,outname+".pkl.gz")
+    print(f"\nSaving output in {out_pkl_file}...")
+    with gzip.open(out_pkl_file, "wb") as fout:
+        cloudpickle.dump(output, fout)
+    print("Done!")
